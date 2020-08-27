5170a4e6a4082bf2199156a4896414b5afb87818
Bagassurahman <bagassurahman2@gmail.com>
2020-08-27 15:50:53 +0700

    First

---
+++ hitungBerat_09_BagasSurahman/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using beratIdeal_09_BagasSurahman;
+
+namespace hitungBerat_09_BagasSurahman
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            beratPriaWanita bpw = new beratPriaWanita();
+            Console.WriteLine("================================");
+            Console.WriteLine("1. Berat Ideal Pria");
+            Console.WriteLine("2. Berat Ideal Wanita");
+            Console.WriteLine("Masukkan Menu Berat Ideal [1/2] = ");
+            int input = int.Parse(Console.ReadLine());
+            Console.WriteLine("================================");
+            if (input == 1)
+            {
+                Console.WriteLine("======Berat Ideal Pria======");
+                bpw.beratIdealPria();
+                Console.WriteLine("============================");
+            }
+            else if (input == 2)
+            {
+                Console.WriteLine("======Berat Ideal Wanita======");
+                bpw.beratIdealWanita();
+                Console.WriteLine("============================");
+            }
+            else
+            {
+                Console.WriteLine("Menu Not Defined");
+            }
+        }
+    }
+}
