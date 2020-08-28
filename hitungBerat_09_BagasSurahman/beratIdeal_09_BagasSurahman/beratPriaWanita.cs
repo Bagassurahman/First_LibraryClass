41378680dc7fe199dd5f140bd92ea17077d16722
Bagassurahman <53173715+Bagassurahman@users.noreply.github.com>
2020-08-28 13:52:14 +0700

    Add files via upload

---
+++ hitungBerat_09_BagasSurahman/beratIdeal_09_BagasSurahman/beratPriaWanita.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace beratIdeal_09_BagasSurahman
+{
+    public class beratPriaWanita
+    {
+        public virtual void beratIdealWanita()
+        {
+            Console.WriteLine("Masukkan Tinggi Badan Anda : ");
+            double a = double.Parse(Console.ReadLine());
+
+            double w = (a - 100) - (a - 100) * 0.15;
+            Console.WriteLine("Beran Tubuh Ideal Anda : " + w);
+
+        }
+
+        public virtual void beratIdealPria()
+        {
+            Console.WriteLine("Masukkan Tinggi Badan Anda : ");
+            double a = double.Parse(Console.ReadLine());
+
+            double p = (a - 100) - (a - 100) * 0.10;
+            Console.WriteLine("Berat Tubuh Ideal Anda : " + p);
+
+        }
+    }
+}
